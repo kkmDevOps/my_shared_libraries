6b9926308d6fc685d74822a1fd06442fa442be3e
kkmDevOps <139893203+kkmDevOps@users.noreply.github.com>
2023-09-22 08:37:34 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,6 +2,6 @@ def call() {
         def mavenHome = tool name: "Maven-3.9.4", type: "maven"
         def mavenPath = "${mavenHome}/bin/mvn"
         sh "${mavenPath} clean package"
-        cd maven-web-app
+
  // sh "mvn clean package"
 }
